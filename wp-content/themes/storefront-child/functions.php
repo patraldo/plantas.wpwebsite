a8b95a5b3437b4de91a85a9d90de50ed07c02535
christophe <cheftech@patraldo.com>
2022-04-03 21:56:07 +0000

    add child theme with Google Fonts

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -1,14 +1,1 @@
 <?php
-
-
-function mytheme_add_woocommerce_support() {
-add_theme_support( 'woocommerce' );
-}
-add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
-
-
-add_filter( 'woocommerce_billing_fields', 'my_optional_fields' );
-function my_optional_fields( $address_fields ) {
-$address_fields['billing_phone']['required'] = false;
-return $address_fields;
-}
